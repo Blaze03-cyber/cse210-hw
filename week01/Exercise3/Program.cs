dc215072ac9cb3ab8bfa1a146f82bbf0a3330bbe
Mickey Blaze <michaelajax2@gmail.com>
2026-05-09 09:56:57 +0100

    Implement guessing game with random number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // For Part 1 and 2, where the user specified the number...
+        // Console.WriteLine("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // For Part 3, where we use a random number...
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        int guess = -1;
+
+        // we could also use a do-while loop here...
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+        }
+
+        Console.WriteLine("Congratulations! You guessed it!");
     }
 }
