91c456cb1ef4637c42e018278ced9fe82c9a25b2
Mickey Blaze <michaelajax2@gmail.com>
2026-05-09 09:59:40 +0100

    Refactor Program.cs to include user prompts and display

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int  squaredNumber = SquaredNumber(userNumber);
+
+        DisplayResults(userName, userNumber, squaredNumber);
+    
+    }
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Michael Ajayi");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("42");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquaredNumber(int userNumber)
+    {
+        Console.WriteLine("1764");
+        int squared = userNumber * userNumber;
+        return squared;
+    }
+
+    static void DisplayResults(string PromptUserName, int PromptUserNumber, int SquaredNumber)
+    {
+        Console.WriteLine($"Hello, {PromptUserName}!");
+        Console.WriteLine($"The number you entered is: {PromptUserNumber}");
+        Console.WriteLine($"The square of that number is: {SquaredNumber}");
     }
 }
