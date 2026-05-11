d6bd59c8cb934427c0a5d9391f09eabef0e94beb
Mickey Blaze <michaelajax2@gmail.com>
2026-05-11 20:13:06 +0100

    Add Journal class with entry management methods

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,24 @@
+public class Journal
+{
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        
+    }
+
+    public void SaveToFile(string filename)
+    {
+        
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        
+    }
+}
